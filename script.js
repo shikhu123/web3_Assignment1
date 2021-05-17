f07cee5a4f82e28fcf19f1091accd7b02fd37b31
shikhu123 <shikhap557@gmail.com>
2021-05-17 13:06:05 -0400

    third commit

--- script.js
+++ script.js
@@ -4,10 +4,47 @@ function clickme()
 
     removeElementID("9");
 
-var inputText = document.getElementsByClassName("text");
+   var inputText = document.getElementsByClassName("text");
 
-for (var i = 0; i < inputText.length; i++) {
-inputText[i].value = "";
-}
+   for (var i = 0; i < inputText.length; i++) 
+   {
+       inputText[i].value = "";
+   }
 }
 
+window.onload = (function () {
+
+    Properties();
+    
+    function Properties() 
+    {
+    
+        var MouseHoverButton = document.getElementsByClassName("btnMouseHover");
+    
+        for (var i = 0; i < MouseHoverButton.length; i++) 
+        {
+
+            MouseHoverButton[i].addEventListener("mousehover", function () 
+            {
+    
+            document.body.style.backgroundColor = "red";
+    
+            })
+    
+        MouseHoverButton[i].addEventListener("mouseleaves", function ()
+        {
+            document.body.style.backgroundColor = "white";
+        })
+    
+        MouseHoverButton[i].addEventListener("click", function ()
+         {
+            var buttonCLone = this.cloneNode(true);
+            document.body.appendChild(buttonCLone);
+    
+    Properties();
+    })
+    }
+    }
+    
+});
+    
